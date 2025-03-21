1dc3e7129e91e015abbb70dcac7be4cc13983f19
Megha-Kumari14 <megha1482003@gmail.com>
2025-03-21 15:46:41 +0530

    Update prduct.ts

--- src/models/prduct.ts
+++ src/models/prduct.ts
@@ -11,3 +11,26 @@ export interface Product{
     Currency: string;
 
 }
+
+export interface Product
+{
+    productID: number;
+    name: string;
+    description: string;
+    sku: string;
+    categoryID: number;
+    categoryName?: string;
+    stockQuantity: number;
+    price: number;
+    currency: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+
+
+export interface Category {
+    categoryID: number;
+    name: string;
+  }
+  
