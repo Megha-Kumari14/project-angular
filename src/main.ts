98c037f3350779fdb1a6524d9a30bd6b7e00e53f
Megha-Kumari14 <megha1482003@gmail.com>
2025-03-21 16:18:06 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,18 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+
+
+
+
+
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
