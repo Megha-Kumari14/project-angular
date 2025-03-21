dab44f3c1109e429f8e8849f6e1197cd874274bc
Megha-Kumari14 <megha1482003@gmail.com>
2025-03-21 16:16:23 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,3 +7,14 @@ export const routes: Routes = [
     {path: 'add', component: ProductFormComponent}
     {path: 'product', redirectTo: '', pathMatch: 'full'}
 ];
+
+
+import { Routes } from '@angular/router';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+
+export const routes: Routes = [
+    {path: '', component: ProductTableComponent},         //maps to ProductTableComponent display list of products
+    {path: 'add', component: ProductFormComponent},       //maps to ProductFormComponent to display the form 
+    {path: 'product', redirectTo: '', pathMatch: 'full'}  //redirects to home page
+];
