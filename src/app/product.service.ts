6b4791a223642536c29a204dd883375c4b6369e7
Megha-Kumari14 <megha1482003@gmail.com>
2025-03-21 16:17:23 +0530

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -25,3 +25,69 @@ export class ProductService {
   checkSkuUnique(sku: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/check-sku`, sku);
 }
+
+
+
+
+
+
+
+
+
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = `${environment.apiUrl}/products`; 
+ 
+
+  constructor(private http: HttpClient) { }
+
+  getProduct(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+  
+  addProduct(product: Product): Observable<Product>{
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+categoryservice.ts:
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Category } from '../../models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiUrl = `${environment.apiUrl}/categories`;  // Adjust based on your API URL
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+}
