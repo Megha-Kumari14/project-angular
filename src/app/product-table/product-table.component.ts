c2c8f05d54c4be1acee846852ebca71624226699
Megha-Kumari14 <megha1482003@gmail.com>
2025-03-25 17:12:33 +0530

    Update product-table.component.ts

--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -1,74 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../models/prduct';
-import { ProductService } from '../product.service';
-import { CommonModule } from '../../../node_modules/@angular/common/index';
+import { Product } from '@models/product';
+import { ProductService } from '@services/product.service';
+import { CategoryService } from '@services/category.service';  
+import { Category } from '@models/category';
+import { Router } from '@angular/router';  
 
 @Component({
   selector: 'product-table',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './product-table.component.html',
-  styleUrl: './product-table.component.css'
-})
-export class ProductTableComponent {
-
-  product: Product[] = [];
-
-  constructor(private productService: ProductService){}
-
-  ngOnit(){
-    this.productService.getProduct().subscribe((data: Product[]) => {
-      this.product = data;
-      console.log(data);
-    })
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../../../models/product';
-import { ProductService } from '../product.service';
-import { CategoryService } from '../category.service';  
-import { Category } from '../../../models/category';  
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';  // Import Router from '@angular/router'
-
-
-@Component({
-  selector: 'product-table',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './product-table.component.html',
-  styleUrls: ['./product-table.component.css']
+  styleUrls: ['./product-table.component.scss']
 })
 export class ProductTableComponent implements OnInit {
 
   products: Product[] = [];
-  categories: Category[] = []; // Storing categories
+  categories: Category[] = []; 
 
   constructor(
     private productService: ProductService,
     private categoryService: CategoryService,
-    private router: Router   // Inject Router here
+    private router: Router  
 
   ) {}
 
   ngOnInit() {
-    // Fetch products
     this.productService.getProduct().subscribe({
       next: (data: Product[]) => {
         console.log('Fetched products:', data);  
@@ -79,7 +33,6 @@ export class ProductTableComponent implements OnInit {
       }
     });
 
-    // Fetch categories
     this.categoryService.getCategories().subscribe({
       next: (categories: Category[]) => {
         console.log('Fetched categories:', categories);
@@ -91,16 +44,13 @@ export class ProductTableComponent implements OnInit {
     });
   }
 
-  // Method to get category name by categoryID
   getCategoryName(categoryID: number): string {
     const category = this.categories.find(cat => cat.categoryID === categoryID);
     return category ? category.name : 'Unknown'; 
   }
 
-  // Navigate to product form when 'Add Product' button is clicked
   onAddProduct(): void {
-    this.router.navigate(['/add']);
+    this.router.navigate(['/product/add']);
+    
   }
-
 }
-
